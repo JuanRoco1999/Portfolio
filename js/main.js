9f1ee2e9c189af380f7cbfccc4d69df44c74eb05
JuanRoco1999 <juanroco1999@gmail.com>
2024-04-22 12:50:38 -0300

    Changes

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ window.onscroll = () => {
             });
         }
     });
-
+        
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100); 
 
